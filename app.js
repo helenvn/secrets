2e43c1f150d66a1f21cda0852a4103c7642211cc
Helen van Niekerk <helenvn@mweb.co.za>
2022-05-19 12:05:27 +0200

    bcrypt version passport has already been installed but not implemented

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
 
-const app = express();      
+const saltRounds = 10;
+
+
+const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -23,7 +26,7 @@ const userSchema = new mongoose.Schema ({
 
 const secret = process.env.SECRET;
 
-userSchema.plugin(encrypt,  {secret: secret, encryptedFields: ["password"] });
+
 
 
 const User = new mongoose.model("User", userSchema);
@@ -38,18 +41,22 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+     newUser.save(function(err){
+       if (err) {
+         console.log(err);
+       }
+       else {
+         res.render("secrets");
+       }
+     });
   });
-   newUser.save(function(err){
-     if (err) {
-       console.log(err);
-     }
-     else {
-       res.render("secrets");
-     }
-   });
+
 
 });
 
@@ -67,9 +74,11 @@ app.post("/login", function(req, res){
     }
     else{
       if (foundUser){
-        if (foundUser.password === password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result == true) {
+            res.render("secrets");
+          }
+        });
       }
     }
   })
